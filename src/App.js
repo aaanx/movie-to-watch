2a016bb2eeb84550b308ef1f95252a468cdeae1b
Anna <anna.walkowiakk@gmail.com>
2020-10-20 17:01:46 +0200

    ADD IMDB API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import MovieSearch from "./components/MovieSearch/MovieSearch.js";
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    movies: []
+  };
+
+  componentDidMount(){
+    axios.get("https://imdb-api.com/en/API/SearchMovie/k_xuaz1j2w/Inception 2010").then(
+      result => {
+        this.setState({
+          isLoaded: true,
+          movies: result.data
+        });
+      },
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  
+  componentDidUpdate() {
+    console.log(this.state.movies.results);
+  }
+
+  render(){
+    const { error, isLoaded, movies } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <MovieSearch/>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
