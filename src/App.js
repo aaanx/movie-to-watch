52e12fb8954fff5586dde9f437c467630939bce5
Anna <anna.walkowiakk@gmail.com>
2020-11-08 15:40:27 +0100

    ADD loading results info text

--- src/App.js
+++ src/App.js
@@ -9,113 +9,117 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 class App extends React.Component {
-constructor(props){
-  super(props);
-  this.handleMovieChange = this.handleMovieChange.bind(this);
-  this.handleSearch = this.handleSearch.bind(this);
-  this.handleAddToWatchList = this.handleAddToWatchList.bind(this);
-  this.state = {
-    error: null,
-    isLoaded: false,
-    movies: [],
-    movieName: "",
-    watchList: JSON.parse(localStorage.getItem('watchList')) || [],
-    watchListStorage: []
-  };
-}
+  constructor(props){
+    super(props);
+    this.handleMovieChange = this.handleMovieChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleAddToWatchList = this.handleAddToWatchList.bind(this);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      movies: [],
+      movieName: "",
+      watchList: JSON.parse(localStorage.getItem('watchList')) || [],
+      watchListStorage: [],
+      loading: ""
+    };
+  }
 
-removeDuplicates(originalArray, prop) {
-  var newArray = [];
-  var lookupObject = {};
+  removeDuplicates(originalArray, prop) {
+    var newArray = [];
+    var lookupObject = {};
 
-  for (var i in originalArray) {
+    for (var i in originalArray) {
     lookupObject[originalArray[i][prop]] = originalArray[i];
   }
 
   for (var j in lookupObject) {
     newArray.push(lookupObject[j]);
   }
-  
-  return newArray;
-}
+    return newArray;
+  }
 
-handleAddToWatchList(movieToAddID){
-  let watchList = this.state.watchList;
-
-  watchList.push(
+  handleAddToWatchList(movieToAddID){
+    let watchList = this.state.watchList;
+    watchList.push(
     this.state.movies.results.find((movie) => {
-    return movie.id == movieToAddID;
+      return movie.id == movieToAddID;
     })
   )
+    let uniqueWatchList = this.removeDuplicates(watchList, "id");
+    this.setState({
+      watchList: uniqueWatchList
+    }) 
+  }
 
-  let uniqueWatchList = this.removeDuplicates(watchList, "id");
-
-  this.setState({
-    watchList: uniqueWatchList
-  }) 
-}
-
-handleMovieChange(e) {
+  handleMovieChange(e) {
     this.setState({
       movieName: e.currentTarget.value
     });
-}
+  }
 
-handleSearch(e) {
+  handleSearch(e) {
     e.preventDefault();
-    axios.get(`https://imdb-api.com/en/API/SearchMovie/k_xuaz1j2w/${this.state.movieName}`).then(
-      result => {
-        this.setState({
-          isLoaded: true,
-          movies: result.data
-        });
-      },
-      error => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    );
-}
+    if(this.state.movieName.length > 0){
+      this.setState({
+        loading: "Loading results..."
+      })
+      axios.get(`https://imdb-api.com/en/API/SearchMovie/k_xuaz1j2w/${this.state.movieName}`).then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            movies: result.data
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+    } else {
+      console.log("Enter movie name");
+    }
+  }
 
-componentDidUpdate() {
-  console.log(this.state.watchList);
-  localStorage.setItem('watchList', JSON.stringify(this.state.watchList));
-}
+  componentDidUpdate() {
+    console.log(this.state.watchList);
+    localStorage.setItem('watchList', JSON.stringify(this.state.watchList));
+  }
 
-render(){
-  const { error, isLoaded, movies } = this.state;
-  // if (error) {
-  //   return <div>Error: {error.message}</div>;
-  // } else {
-    return (
-      <Router>
-      <div className="App">
-        <Navbar numOfmovies={this.state.watchList.length}/>
-        <Switch>
-          <Route path="/watch-list">
-            <WatchList watchList={this.state.watchList} />
-          </Route> 
-          <Route path="/">
-            <MovieSearch 
-              movieName={this.state.movieName} 
-              handleMovieChange={this.handleMovieChange} 
-              handleSearch={this.handleSearch}  
-            />
-            {isLoaded ? 
-            <SearchList 
-              movies={this.state.movies.results}
-              handleAddToWatchList={this.handleAddToWatchList}
-            /> : "" }
-          </Route>
-        </Switch>
-        
-      </div>
-      </Router>
-    );
+  render(){
+    const { error, isLoaded, movies, loading } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return (
+        <Router>
+        <div className="App">
+          <Navbar numOfmovies={this.state.watchList.length}/>
+          <Switch>
+            <Route path="/watch-list">
+              <WatchList watchList={this.state.watchList} />
+            </Route> 
+            <Route path="/">
+              <MovieSearch 
+                movieName={this.state.movieName} 
+                handleMovieChange={this.handleMovieChange} 
+                handleSearch={this.handleSearch}  
+              />
+              {isLoaded ? 
+              <SearchList 
+                movies={this.state.movies.results}
+                handleAddToWatchList={this.handleAddToWatchList}
+              /> : loading }
+            </Route>
+          </Switch>
+          
+        </div>
+        </Router>
+      );
+    }
   }
 }
-// }
 
 export default App;
