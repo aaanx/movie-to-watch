e0a77e3d93da32d47907a9055231a7014e59ea3e
Anna <anna.walkowiakk@gmail.com>
2020-11-08 15:24:15 +0100

    ADD enter movie name placeholder

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -7,7 +7,7 @@ function MovieSearch(props) {
         <div className="movie-search">
             <p className="movie-search__text">Search for a movie to add it to your watch list</p>
             <form className="movie-search__form">
-                <input type="text" name={props.movieName} onChange={props.handleMovieChange} className="movie-search__form__input" />
+                <input type="text" placeholder="Enter movie name" name={props.movieName} onChange={props.handleMovieChange} className="movie-search__form__input" />
                 <input type="submit" value="Search" onClick={props.handleSearch} className="movie-search__form__button"/>
             </form>
         </div>
